e6411cb509fcb21d00b27114c4f6d0890a430a65
DarioDiGulio <dariodigulio@gmail.com>
2022-07-09 20:18:51 -0300

    hello world api

--- api/src/main/java/com/juanmanuelcastillo/moneymanager/MoneymanagerApplication.java
+++ api/src/main/java/com/juanmanuelcastillo/moneymanager/MoneymanagerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoneymanagerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MoneymanagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MoneymanagerApplication.class, args);
+    }
 }
