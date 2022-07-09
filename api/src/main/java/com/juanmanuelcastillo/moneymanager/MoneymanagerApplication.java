885309afa0029fbc212999ae8db797921eacf44f
DarioDiGulio <dariodigulio@gmail.com>
2022-07-09 19:52:36 -0300

    setup api

---
+++ api/src/main/java/com/juanmanuelcastillo/moneymanager/MoneymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.juanmanuelcastillo.moneymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymanagerApplication.class, args);
+	}
+
+}
